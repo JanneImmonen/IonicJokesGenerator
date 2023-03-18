5549096d48ca1c19b6e883a82b6a9971d3c102bc
Developer-Rathod <immonenjanne@icloud.com>
2023-03-18 13:40:17 +0200

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  joke: any;
 
-  constructor() {}
+  constructor(private apiService: ApiService) {}
 
+  ngOnInit() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    const url = 'https://v2.jokeapi.dev/joke/Programming';
+    this.apiService.getData(url).subscribe(
+      response => {
+        this.joke = response;
+        console.log(this.joke);
+      },
+      error => {
+        console.error('Error fetching data:', error);
+        // You can display an error message or handle it as needed
+      }
+    );
+  }
 }
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getData(url: string) {
+    return this.httpClient.get(url);
+  }
+}
